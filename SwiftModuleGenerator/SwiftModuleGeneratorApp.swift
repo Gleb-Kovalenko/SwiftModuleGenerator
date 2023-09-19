ff5ae133cf4246821ea1eb9ee57c4d1685826cc4
Gleb Kovalenko <yourpendos@gmail.com>
2023-09-19 19:24:54 +0300

    Initial Commit

---
+++ SwiftModuleGenerator/SwiftModuleGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftModuleGeneratorApp.swift
+//  SwiftModuleGenerator
+//
+//  Created by Gleb Kovalenko on 19.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftModuleGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
